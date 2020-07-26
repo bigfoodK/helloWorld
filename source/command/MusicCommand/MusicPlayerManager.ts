50060ee6a1cf2a3e3570894d2138d83450d16204
bigfoodk <satcom1600@hanmail.net>
2020-07-26 15:01:23 +0900

    Fix player to play next music on end music

--- source/command/MusicCommand/MusicPlayerManager.ts
+++ source/command/MusicCommand/MusicPlayerManager.ts
@@ -1,15 +1,16 @@
 import MusicPlayer from './MusicPlayer';
+import { Guild } from 'discord.js';
 
 export default class MusicPlayerManager {
   private musicPlayers: Map<string, MusicPlayer> = new Map();
 
-  public get(guildId: string) {
-    const musicPlayer = this.musicPlayers.get(guildId);
+  public get(guild: Guild) {
+    const musicPlayer = this.musicPlayers.get(guild.id);
     if (musicPlayer) {
       return musicPlayer;
     }
-    const newMusicPlayer = new MusicPlayer();
-    this.musicPlayers.set(guildId, newMusicPlayer);
+    const newMusicPlayer = new MusicPlayer(guild);
+    this.musicPlayers.set(guild.id, newMusicPlayer);
     return newMusicPlayer;
   }
 }
